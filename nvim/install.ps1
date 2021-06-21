688b899bab9511ca848bcbe27480f64d0cfd4e69
jm33-m0 <jm33.m0@gmail.com>
2021-06-21 20:27:03 +0800

    nvim for windows

---
+++ nvim/install.ps1
@@ -0,0 +1,12 @@
+Invoke-WebRequest 'https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.ps1' -OutFile installer.ps1
+# Allow to run third-party script
+Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser
+# install dein package manager
+./installer.ps1 ~/.dein
+
+$nvimrc = "$env:LOCALAPPDATA\nvim\init.vim"
+$gnvimrc = "$env:LOCALAPPDATA\nvim\ginit.vim"
+Copy-Item -Path .\win\init.vim -Destination $nvimrc -Force
+Copy-Item -Path .\win\ginit.vim -Destination $gnvimrc -Force
+Write-Host "Add required config to $nvimrc"
+notepad.exe $nvimrc
