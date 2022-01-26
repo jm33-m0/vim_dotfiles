06607c248b0c1e0e10e9c8c4b4d01c4c1f4aaddc
jm33-m0 <jm33@jm33.me>
2022-01-26 11:28:06 +0800

    update install.sh

--- nvim/install.sh
+++ nvim/install.sh
@@ -6,27 +6,51 @@ command -v 'nvim' || {
 }
 
 # python binding
-command -v python3 && python3 -m pip install neovim python-language-server pyright pylint yapf autopep8 --user
-command -v python2 && python2 -m pip install neovim python-language-server pylint yapf autopep8 --user
+(command -v python3 && ! python3 -m pip list | grep pynvim) && {
+    python3 -m pip install neovim python-language-server pyright pylint yapf autopep8 --user
+}
+(command -v python2 && ! python2 -m pip list | grep pynvim) && {
+    python2 -m pip install neovim python-language-server pylint yapf autopep8 --user
+}
 
 # install dein.nvim
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh >installer.sh
-sh ./installer.sh ~/.dein
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh -o /tmp/installer.sh
+sh /tmp/installer.sh ~/.dein && rm -f /tmp/installer.sh
 
 # install dependencies
 command -v apt && {
     echo -e "\n\napt detected, trying to install dependencies"
     sudo apt install ripgrep fzf -y            # required
-    sudo apt install nodejs npm                # nodejs, required by many plugins
     sudo apt install golang gopls              # golang
     sudo apt install clang clangd clang-format # c
     sudo apt install shellcheck                # bash
+    sudo apt install snapd                     # snap
 }
-command -v npm && sudo npm install -g yarn
+
 command -v snap && {
+    echo -e "\n\nsnap detected, trying to install more utilities"
     sudo snap install shfmt
+    sudo snap install node --classic
+    sudo snap install lazygit
 }
-command -v go && {
+
+# install nodejs via apt if snap is unavailable
+(command -v snap && ! command -v node) && {
+    echo -e "\n\nnodejs not found, trying to install from apt"
+    sudo apt install nodejs npm
+}
+
+# nodejs related packages
+test_ro=/usr/local/bin/nvim-test
+(command -v npm && sudo touch "$test_ro") && {
+    echo -e "\n\nnodejs found, trying to install required modules"
+    sudo npm install -g js-beautify
+    sudo npm install -g yarn
+    [[ -f "$test_ro" ]] && sudo rm -f "$test_ro"
+}
+
+(command -v go && ! command -v shfmt) && {
+    echo -e "\n\ninstalling shfmt via go get"
     go install mvdan.cc/sh/v3/cmd/shfmt@latest || echo "shfmt failed to install, figure it out yourself"
 }
 
